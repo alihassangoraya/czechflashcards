9f2b1286e829bd363b2981d6d828a16a8c9332d1
Ali Hassan Sittar <alihassangoraya@gmail.com>
2026-06-30 16:40:19 +0200

    Simplify study screen content props

--- apps/mobile/src/features/study/screens/StudyScreen.tsx
+++ apps/mobile/src/features/study/screens/StudyScreen.tsx
@@ -5,60 +5,20 @@ import { StudyProgress } from "../components/StudyProgress";
 import type { StudyScreenProps } from "../studyScreenTypes";
 
 export function StudyScreen({
-  current,
-  settings,
-  savedCardIds,
-  revealed,
-  flipping,
-  grading,
-  swipeDirection,
-  lastReviewCard,
   sessionReviews,
   sessionTarget,
   reviewedToday,
   dailyGoal,
   sessionProgress,
-  dragX,
-  flipProgress,
-  cardRotation,
-  panHandlers,
-  reviewInterval,
   onBack,
   onOpenGrammar,
-  onFlipCard,
-  onToggleSaved,
-  onManageDecks,
-  onEditCard,
-  onCompleteSwipe,
-  onUndoLastReview,
-  onGrade
+  ...contentProps
 }: StudyScreenProps) {
   return (
     <>
       <StudyHeader onBack={onBack} onOpenGrammar={onOpenGrammar} />
       <StudyProgress sessionReviews={sessionReviews} sessionTarget={sessionTarget} reviewedToday={reviewedToday} dailyGoal={dailyGoal} sessionProgress={sessionProgress} />
-      <StudyContent
-        current={current}
-        settings={settings}
-        savedCardIds={savedCardIds}
-        revealed={revealed}
-        flipping={flipping}
-        grading={grading}
-        swipeDirection={swipeDirection}
-        lastReviewCard={lastReviewCard}
-        dragX={dragX}
-        flipProgress={flipProgress}
-        cardRotation={cardRotation}
-        panHandlers={panHandlers}
-        reviewInterval={reviewInterval}
-        onFlipCard={onFlipCard}
-        onToggleSaved={onToggleSaved}
-        onManageDecks={onManageDecks}
-        onEditCard={onEditCard}
-        onCompleteSwipe={onCompleteSwipe}
-        onUndoLastReview={onUndoLastReview}
-        onGrade={onGrade}
-      />
+      <StudyContent {...contentProps} />
     </>
   );
 }
